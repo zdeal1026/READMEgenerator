2d740bc6821b0b5c69d46dcb84d5a121432a5484
Zdeal <jedimastershake@gmail.com>
2021-06-28 22:36:11 -0400

    updating java

--- index.js
+++ index.js
@@ -1,72 +1,83 @@
 // TODO: Include packages needed for this application
-const package = require('inquirer')
 const fs = require('fs')
+const path = require('path')
+const inquirer = require('inquirer')
 const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
-        name: 'Title',
+        name: 'title',
         message: 'Enter title of project',
-        validate: (title) => {if (title) {return true} else {return 'Please enter data'}},
     },
     {
         type: 'input',
-        name: 'Table of Contents',
-        message: 'add table of contents',
-        validate: (title) => {if (title) {return true} else {return 'Please enter data'}},
-    },
-    {
-        type: 'input',
-        name: 'Description',
+        name: 'description',
         message: 'Please enter short description of project',
-        validate: (title) => {if (title) {return true} else {return 'Please enter data'}},
     },
     {
         type: 'input',
-        name: 'Installation',
-        message: 'Please enter Installation information',
-        validate: (title) => {if (title) {return true} else {return 'Please enter data'}},
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
     },
     {
         type: 'input',
-        name: 'Usage',
-        message: 'How does the user use this application?',
-        validate: (title) => {if (title) {return true} else {return 'Please enter data'}},
+        name: 'usage',
+        message: 'Provide instructions and examples for use',
     },
     {
         type: 'list',
-        name: 'License',
+        name: 'license',
         message: 'What kind of license do you need?',
         choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None',]
     },
     {
         type: 'input',
-        name: 'Credits',
-        message: 'Where did you get the matierial to complete this project?',
+        name: 'contributing',
+        message: 'allow other users to contribute to the application using industry standard Contributor Convenant ',
+        default: 'Allow users to contribute to the application using Contributor Covenant (https://www.contributor-covenant.org/)',
     },
     {
         type: 'input',
-        name: 'Test',
-        message: 'How is ',
-        validate: (title) => {if (title) {return true} else {return 'Please enter a Title'}},
+        name: 'test',
+        message: 'How do you run the test from the command line? ',
+        default: 'npm run test'
     },
     {
         type: 'input',
-        name: 'Questions',
-        message: 'If you have any questions please contact me by email at....',
-        validate: (title) => {if (title) {return true} else {return 'Please enter a Title'}},
+        name: 'email',
+        message: 'enter email',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'enter github repo',
+    },
+    {
+        type: 'input',
+        name: 'links',
+        message: 'additional links',
     }
+
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+//^^ return fileSystem and function *writeTofile using path.join to join the "current working directory" to the file
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((inquirerResponses) => {
+      console.log('applying qustions to README.md..');
+      //message in command line to show readme is being generated
+      writeToFile('README.md', generateMarkdown({ ...inquirerResponses }));
+      //calling writeToFile function where README.md is destination file and logs info using generateMarkdown file
+    });
+  }
+
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,91 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return '';
+}
 
-// TODO: Create a function that returns the license link
+// Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return `\n* [License](#license)\n`;
+  }
+  return '';
+}
 
-// TODO: Create a function that returns the license section of README
+// Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `## License
 
-// TODO: Create a function to generate markdown for README
+${license} license used.`;
+  }
+  return '';
+}
+
+// Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
+
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+${renderLicenseLink(data.license)}
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+* [Contact](#contact)
+
+* [Links](#links)
+
+## Description
+
+${data.description}
+
+## Installation
+
+To add node modules run 
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+  
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+To run tests, run the following command:
+
+\`\`\`
+${data.test}
+\`\`\`
+
+## Questions
+
+For any questions please use the contact information below
+
+## Contact
+ email ${data.email},  github repo ${data.github}](https://github.com/${data.github}/).
+
+## Links
+
+${data.links}
 
 `;
 }
